75e32fc2fc6c34c21020d1d132e7a4b61283696f
andrewshowers <andrew.showers@hope.edu>
2022-10-18 18:46:12 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+# This is a sample Python script.
+
+print('|\\')
+print('| \\',  '           / |')
+print('|  \\',  '         /  |')
+print('|   \\',  '       /   |')
+print(' \\   \\',  '     /    |')
+print('  \\   \\',  '   /    /')
+print('   \\   \\  ', u'\u2500' * 4)
+print('    \\   \\')
+print('    ', u'\u2500' * 4)
+
+
+
+
+
